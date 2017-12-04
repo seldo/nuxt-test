081fc3634fc526860f7c3b17d7c3d64a3559b3d4
Laurie Voss <github@seldo.com>
2017-12-03 20:13:42 -0800

    Moar content

---
+++ pages/3-basic-skills/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Basic Progamming Skills</h1>
+    <h2>The stuff everybody really should know</h2>
+    <p>There should be a lot here but there isn't.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'page'
+}
+</script>
+
+<style>
+</style>
