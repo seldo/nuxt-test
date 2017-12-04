4e036910281c93281ec3f56708d8c59e1a6b6911
Laurie Voss <github@seldo.com>
2017-12-03 17:18:13 -0800

    More history

--- pages/1-intro/index.vue
+++ pages/1-intro/index.vue
@@ -15,14 +15,15 @@
     <section>
       <h2>The Long Version</h2>
       <p>In 2014, I was at JSConf US. Because JavaScript conferences are more fun than other conferences, I was in the pool. I was talking to <a href="https://twitter.com/HarleyKwyn">Kwyn</a>, who I had just met, about various web development topics, and I got into a rant about rich apps vs. flat sites. We kept talking, and I kept launching into one rant after another about things junior web developers should know but didn't, stuff that we as web developers had worked out 10 and 15 years ago.</p>
-      <p>Kwyn found my rants entertaining, and had just graduated from Hack Reactor. She observed that while all of these things were well known to people who'd been in the industry for years, graduates of boot camps like Hack Reactor had never heard these things. They knew how to use the latest tech, but they had much less background on why. She suggested I come to Hack Reactor and give a talk about all those sorts of things. I spontaneously agreed, and decided to call it Shit Everybody Knows.</p>
-      <p>A few weeks later the day of the talk rolled around and, predictably, I hadn't really thought about what I was going to say.</p>
+      <p>Kwyn found my rants entertaining, and had just graduated from Hack Reactor. She observed that while all of these things were well known to people who'd been in the industry for years, graduates of boot camps like Hack Reactor had never heard these things. They knew how to use the latest tech, but they had not been around when very old arguments had happened, so they often didn't have background. She suggested I come to Hack Reactor and give a talk about all those sorts of things. I spontaneously agreed, and decided to call it Shit Everybody Knows.</p>
+      <p>A few weeks later the day of the talk rolled around and, predictably, I hadn't really thought about what I was going to say. So, panicked, I just began throwing anything I could think of into a slide deck. I asked people on Twitter what sort of topics junior devs didn't know about, and that they got annoyed when they had to re-explain, and that was a rich vein of material. I also threw in my personal bugbears, like flat sites and various databases.</p>
+      <p>With no time to practice, I had no idea how long this talk was going to be. I showed up at Hack Reactor and just launched into it, spicing it liberally with anecdotes and profanity. Two hours later I finished to a round of applause, and the talk was born.</p>
+      <p>Hack Reactor invited me back to give the talk again and again -- ten times so far, and I hope they continue to do so. I've also given the talk to </p>
     </section>
   </div>
 </template>
 
 <script>
-
 export default {
   layout: 'page'
 }
