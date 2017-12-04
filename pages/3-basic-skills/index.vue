0b239f7dce47868378677f628274429dc8bec14e
Laurie Voss <github@seldo.com>
2017-12-03 21:29:34 -0800

    Typo

--- pages/3-basic-skills/index.vue
+++ pages/3-basic-skills/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Basic Progamming Skills</h1>
+    <h1>Basic Programming Skills</h1>
     <h2>The stuff everybody really should know</h2>
     <p>There should be a lot here but there isn't.</p>
   </div>
