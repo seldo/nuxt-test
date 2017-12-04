80f8b1318ebf1053559419a7d0b8d64d07ddad9f
Laurie Voss <github@seldo.com>
2017-12-03 17:16:59 -0800

    Table of contents

---
+++ components/toc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="topic in topics"><a :href="'/' + topic.slug">{{topic.name}}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import topics from './topics'
+
+export default {
+  data () {
+    return {
+      topics
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ components/topics.js
@@ -0,0 +1,18 @@
+let topics = [
+  {
+    name: 'Home',
+    slug: '' },
+  {
+    name: 'Intro',
+    slug: '1-intro'
+  },
+  {
+    name: 'Dunning-Kruger',
+    slug: '2-dunning-kruger'
+  },
+  {
+    name: 'HTML & CSS',
+    slug: '3-html-css'
+  }
+]
+export default topics
