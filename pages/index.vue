0ea4b17309fc8554d6bee0d5d09992825d48a676
Laurie Voss <github@seldo.com>
2017-12-03 17:18:27 -0800

    Better instructions and intro

--- pages/index.vue
+++ pages/index.vue
@@ -4,48 +4,84 @@
       <h1>Stuff Everybody Knows About Web Development</h1>
       <h2>(except you)</h2>
       <h3>The companion website to the Stuff Everybody Knows talk by <a href="https://twitter.com/seldo">@seldo</a>.</h3>
+      <p><a id="start" class="link-deeper" href="/1-intro">Start reading</a></p>
       <div class="instructions">
-        If you don't know where to start, head on over to the <a href="/1-intro">introduction</a> and all will be explained. Otherwise, pick a section to visit.
+        <h4>Instructions</h4>
+        <ul>
+          <li>This site is divided into a number of high-level topics, split into self-contained sections.</li>
+          <li>To read the whole site sequentially, just click ⬇️ every time you reach the end of a section.</li>
+          <li>You can consider ⬇️ going "deeper" on a topic. When you've gone as deep as you can, you'll automatically go to the next topic.</li>
+          <li>If you get bored of a topic, use ➡️ to skip to the next topic.</li>
+          <li>Naturally, you can use ⬆️ to go back up one level and ⬅️ to go back one topic.</li>
+        </ul>
       </div>
-      <Navigation class="frontnav"/>
+      <div class="toc">
+        <h3>Full topic list</h3>
+        <ToC />
+      </div>
+      <div>
+        <h3>This is an alpha</h3>
+        <p>This site is a work in progress. It's ugly as hell. The layout, navigation and URLs cannot be trusted to stay constant, and most of the content is still missing.</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+#start {
+  display: block;
+  margin-top: 1em;
+  font-size: 150%;
+}
+/* small screens are default */
 .frontpage-flex {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-.frontpage {
-  max-width: 40em;
+  margin-top: 1em;
+  margin-left: 1em;
+  margin-right: 1em;
 }
 .frontpage h1 {
-  font-size: 400%;
+  font-size: 200%;
 }
-.frontpage h2 {
-  font-size: 400%;
-}
-.frontnav {
-  margin: 1em;
-  padding: 0.5em;
-  background-color: #efefef;
+.frontpage h3 {
+  margin-top: 1em;
 }
 .instructions {
-  margin-top: 4em;
+  margin-top: 2em;
+  text-align: left;
+}
+.toc {
+  text-align: left;
+  margin-top: 1em;
+}
+/* desktop screen sizes */
+@media (min-width:600px) {
+  .frontpage {
+    max-width: 40em;
+  }
+  .frontpage h1 {
+    font-size: 400%;
+  }
+  .frontpage h2 {
+    font-size: 400%;
+  }
+  .frontpage h3 {
+    margin-top: 1em;
+  }
 }
 </style>
 
 <script>
-import Navigation from '~/components/navigation.vue'
+import ToC from '~/components/toc'
 
 export default {
   layout: 'welcome',
   components: {
-    Navigation
+    ToC
   }
 }
 </script>
